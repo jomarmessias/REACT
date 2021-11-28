7db590e105f09daf3a59901643150fc03788dcf4
jomarmessias <jomar.messias@gmail.com>
2021-11-28 14:48:26 -0400

    Commit Inicial - Configuracoes

---
+++ my-app/src/components/List.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List () {
+
+return <ul>
+
+</ul>
+
+} 
+
+export default List
---
+++ my-app/src/views/Home.view.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.view.css';
+
+
+function HomeView () {
+
+ const name = 'Visitante'
+ 
+ return <div className = "HomeView">
+        Seja Bem Vindo, {name}. 
+        <p className = "par">Somente para informar que nossa Página está em Construção!!</p>
+    </div>
+
+}
+
+export default HomeView
